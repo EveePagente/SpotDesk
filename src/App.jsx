f1b5a73d5bb7db62e217c5d178394a5b31d7177d
EveePagente <pagenteevelyn@gmail.com>
2024-04-02 21:28:04 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+
+  return (
+    <>
+    <div className="px-20">
+        <Button>Hello World</Button>
+      </div>
+    </>
+  )
+}
+
+export default App
