95cef0793d33e594f69f30504ce0a785523487b4
alona pegarit <alonajp.pegarit@gmail.com>
2024-04-06 09:25:57 +0800

    Email Verification

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
+import { Mail } from "lucide-react"
 import { Button } from "./components/ui/button";
 
 function App() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <div className="border border-gray-300 p-24 rounded-none shadow-md flex flex-col items-center">
-       <h1 className="text-2xl font-bold pb-4 mb-6">Verify your email address</h1>
-       <p className="text-sm font-semibold mb-2">Please take a second to make sure we have your correct email address.</p>
-       <p className="text-sm font-semibold mb-8">Verify this email address by clicking the button below.</p>
-      <Button className="w-200 h-10 text-sm bg-indigo-700">Verify Email Address</Button>
-    </div>
+      <div className="w-auto h-auto border border-gray-300 p-20 rounded-lg shadow-md flex flex-col items-center">
+        <img className="h-auto w-32 pb-4 self-center mb-8" src="../src/assets/text-dark.png"></img>
+        <h1 className="text-3xl font-semibold mb-10">Verify your email address</h1>
+        <p className="text-medium font-semibold mb-4">Welcome to SpotDesk!</p>
+        <p className="text-medium font-semibold mb-8">To start your desk reservation, please verify your email address by clicking the button below.</p>
+        <Button className="w-200 h-10 text-sm bg-black"><Mail className="mr-2 h-4 w-4" />Verify Email Address</Button>
+      </div>
     </div>
   );
 }
 
 export default App;
-
-
